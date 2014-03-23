fc8cdf210912cf91160706409821586d38719942
Roy Van Liew <vanliewroy@gmail.com>
2014-03-23 13:10:09 -0400

    Working login and logoff

--- HW6/hw6.cpp
+++ HW6/hw6.cpp
@@ -2,7 +2,7 @@
  * File:   hw6.cpp
  * Author: Roy Van Liew and Saqib Zahid
  *
- * Last updated on March 23rd, 2014, 10:05 AM
+ * Last updated on March 23rd, 2014, 1:08 PM
  */
 
 #include <iostream>
@@ -19,10 +19,14 @@ class ComputerLabs
 public:
     ComputerLabs(); // Default constructor
     ~ComputerLabs(); // Destructor
-    void show_labs();
+    void show_labs(); // Finished. Outputs the current status of the labs.
     void login();
     void logoff();
     void search_user();
+    void addLab();
+    void addComp();
+    void removeLab();
+    void removeComp();
     
 private:
     // 1st dimension is lab number, 2nd dimension is user ID (computer).
@@ -81,11 +85,81 @@ void ComputerLabs::show_labs() {
     
 }
 
+void ComputerLabs::login(){
+    
+    string userID;
+    int lab, comp;
+    
+    // Get User ID, 5 characters long.
+    do {
+        cout << "Enter User ID. It must be exactly 5 characters." << endl;
+        cin >> userID;
+    } while ( userID.length() != 5 );
+    
+    // Get the Lab and Computer Station.
+    // These must be within the bounds of the vector.
+    do {
+        cout << "Specify Lab Number." << endl;
+        cin >> lab;
+        cout << "Specify Computer Station." << endl;
+        cin >> comp;
+        if ( ( lab > labs.size() || comp > labs[lab].size() ) || ( lab < 0 || comp < 0 ) )
+            cout << "Invalid lab or computer station." << endl;
+    } while( ( lab > labs.size() || comp > labs[lab].size() ) || ( lab < 0 || comp < 0 ) );
+    
+    // Assign the ID to that spot in the vector.
+    labs[lab][comp] = userID;
+    
+}
+
+void ComputerLabs::logoff(){
+    
+    string userID;
+    int lab, comp;
+    int d1 = labs.size(); // First dimension of labs vector
+    int d2; // Second dimension of labs vector
+    int i, j; // for loop counters.
+    
+    // Get User ID, 5 characters long.
+    do {
+        cout << "Enter User ID. It must be exactly 5 characters." << endl;
+        cin >> userID;
+    } while ( userID.length() != 5 );
+    
+    // Search through the labs.
+    for( i = 0; i < d1; i++){
+        // Here d2 is for the number of computers in that lab.
+        d2 = labs[i].size();
+        for( j = 0; j < d2; j++ ){
+            // If the user is found, log that user off.
+            if( labs[i][j] == userID ){
+                labs[i][j] = "Empty"; // Indicate that the user logged off.
+                cout << "User " << userID << " found at Lab " << i << " Station " << j << "." << endl;
+                cout << "User " << userID << " successfully logged off." << endl;
+                return;
+            }
+        }
+    }
+    
+    // If we get here that means the user was not found.
+    cout << "This user is not logged in." << endl;
+    return;
+    
+}
+
 int main() {
     
-    ComputerLabs compLabs;
-    compLabs.show_labs();
-
+    ComputerLabs lab;
+    lab.show_labs();
+    
+    cout << "Attempting a login." << endl;
+    lab.login();
+    lab.show_labs();
+    
+    cout << "Attempting a logoff." << endl;
+    lab.logoff();
+    lab.show_labs();
+    
     return 0;
 }
 
